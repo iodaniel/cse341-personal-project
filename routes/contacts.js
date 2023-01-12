714541f3a7710700b92f299eaba5a8fb39a46b41
iodaniel <iodaniel@icloud.com>
2023-01-11 20:00:44 -0700

    CRUD

--- routes/contacts.js
+++ routes/contacts.js
@@ -7,6 +7,12 @@ router.get('/', controllerContacts.getAll);
 
 router.get('/:id', controllerContacts.getSingle);
 
+router.post('/', controllerContacts.createContact);
+
+router.put('/:id', controllerContacts.updateContact);
+
+router.delete('/:id', controllerContacts.deleteContact);
+
 
 
 module.exports = router;
